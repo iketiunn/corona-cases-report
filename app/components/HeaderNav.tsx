4689fc2596eb2b12243b7b7217c3e5499adf0176
iketiunn <ike.chang@sideeffect.dev>
2020-10-09 10:47:29 +0800

    Share screenshot file

--- app/components/HeaderNav.tsx
+++ app/components/HeaderNav.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
+import * as Sharing from "expo-sharing";
 import { Appbar } from "react-native-paper";
+import { captureScreen } from "react-native-view-shot";
 
 interface Props {
   title: string;
@@ -10,7 +12,17 @@ export default function RegionScreen({ title }: Props) {
       <Appbar.Content title={title}></Appbar.Content>
       <Appbar.Action
         icon="share-variant"
-        onPress={() => console.log("Pressed share")}
+        onPress={async () => {
+          try {
+            const screenUrl = await captureScreen({
+              format: "jpg",
+              quality: 0.8,
+            });
+            await Sharing.shareAsync(screenUrl);
+          } catch (error) {
+            console.error(error);
+          }
+        }}
       />
     </Appbar.Header>
   );
